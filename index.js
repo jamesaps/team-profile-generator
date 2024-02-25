3ff57cc8bf9735508a03d56c507521385b484de7
James Stott <japstott@gmail.com>
2024-02-25 23:48:52 +0000

    refactor module imports to ESM for information gathering

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+import * as fs from "fs/promises";
+import path from "path";
+import inquirer from "inquirer";
+import Manager from "./lib/Manager.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
+import render from "./src/page-template.js";
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
-
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
